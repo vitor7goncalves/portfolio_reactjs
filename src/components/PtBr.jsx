d8c3d9b3d0947faa755cbdd698ad484d33ab7fd5
vitor7goncalves <you@example.com>
2024-02-03 00:53:34 -0300

    seção projetos

--- src/components/PtBr.jsx
+++ src/components/PtBr.jsx
@@ -1,4 +1,5 @@
 import "./Body.css";
+import React, { useState, useEffect } from "react";
 
 import me from "../assets/Photo/me.png"
 import csharp from "../assets/Icon/C Sharp.png"
@@ -6,9 +7,77 @@ import photoshop from "../assets/Icon/photoshop.png"
 import unity from "../assets/Icon/unity.png"
 import javascript from "../assets/Icon/javascript.png"
 import html from "../assets/Icon/html-5.png"
+import img00 from "../assets/Background/Anim/quadro0000.png";
+import img01 from "../assets/Background/Anim/quadro0001.png";
+import img02 from "../assets/Background/Anim/quadro0002.png";
+import img03 from "../assets/Background/Anim/quadro0003.png";
+import img04 from "../assets/Background/Anim/quadro0004.png";
+import img05 from "../assets/Background/Anim/quadro0005.png";
+import img06 from "../assets/Background/Anim/quadro0006.png";
+import img07 from "../assets/Background/Anim/quadro0007.png";
+import img08 from "../assets/Background/Anim/quadro0008.png";
+import img09 from "../assets/Background/Anim/quadro0009.png";
+import img10 from "../assets/Background/Anim/quadro0010.png";
+import img11 from "../assets/Background/Anim/quadro0011.png";
+import img12 from "../assets/Background/Anim/quadro0012.png";
+import img13 from "../assets/Background/Anim/quadro0013.png";
+import img14 from "../assets/Background/Anim/quadro0014.png";
+import img15 from "../assets/Background/Anim/quadro0015.png";
+import img16 from "../assets/Background/Anim/quadro0016.png";
+import img17 from "../assets/Background/Anim/quadro0017.png";
+import img18 from "../assets/Background/Anim/quadro0018.png";
+import img19 from "../assets/Background/Anim/quadro0019.png";
+import img20 from "../assets/Background/Anim/quadro0020.png";
+import img21 from "../assets/Background/Anim/quadro0021.png";
+import img22 from "../assets/Background/Anim/quadro0022.png";
+import img23 from "../assets/Background/Anim/quadro0023.png";
+import img24 from "../assets/Background/Anim/quadro0024.png";
+import img25 from "../assets/Background/Anim/quadro0025.png";
+import img26 from "../assets/Background/Anim/quadro0026.png";
+import img27 from "../assets/Background/Anim/quadro0027.png";
+import img28 from "../assets/Background/Anim/quadro0028.png";
+import img29 from "../assets/Background/Anim/quadro0029.png";
+import img30 from "../assets/Background/Anim/quadro0030.png";
+import img31 from "../assets/Background/Anim/quadro0031.png";
+import img32 from "../assets/Background/Anim/quadro0032.png";
+import img33 from "../assets/Background/Anim/quadro0033.png";
+import img34 from "../assets/Background/Anim/quadro0034.png";
+import img35 from "../assets/Background/Anim/quadro0035.png";
+import img36 from "../assets/Background/Anim/quadro0036.png";
+import img37 from "../assets/Background/Anim/quadro0037.png";
+import img38 from "../assets/Background/Anim/quadro0038.png";
+import img39 from "../assets/Background/Anim/quadro0039.png";
+import img40 from "../assets/Background/Anim/quadro0040.png";
+import img41 from "../assets/Background/Anim/quadro0041.png";
+import img42 from "../assets/Background/Anim/quadro0042.png";
+import img43 from "../assets/Background/Anim/quadro0043.png";
+import img44 from "../assets/Background/Anim/quadro0044.png";
+import img45 from "../assets/Background/Anim/quadro0045.png";
+import img46 from "../assets/Background/Anim/quadro0046.png";
+import img47 from "../assets/Background/Anim/quadro0047.png";
+import img48 from "../assets/Background/Anim/quadro0048.png";
+import img49 from "../assets/Background/Anim/quadro0049.png";
+import img50 from "../assets/Background/Anim/quadro0050.png";
+import img51 from "../assets/Background/Anim/quadro0051.png";
+import img52 from "../assets/Background/Anim/quadro0052.png";
+import img53 from "../assets/Background/Anim/quadro0053.png";
+import img54 from "../assets/Background/Anim/quadro0054.png";
+import img55 from "../assets/Background/Anim/quadro0055.png";
+import img56 from "../assets/Background/Anim/quadro0056.png";
+import img57 from "../assets/Background/Anim/quadro0057.png";
+
+const imagePaths = [
+    img00, img01, img02, img03, img04, img05, img06, img07, img08, img09,
+    img10, img11, img12, img13, img14, img15, img16, img17, img18, img19,
+    img20, img21, img22, img23, img24, img25, img26, img27, img28, img29,
+    img30, img31, img32, img33, img34, img35, img36, img37, img38, img39,
+    img40, img41, img42, img43, img44, img45, img46, img47, img48, img49,
+    img50, img51, img52, img53, img54, img55, img56, img57
+];
 
 
 export const PtBr = () => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     var textText = { 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '' };
     var languageText = { 0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '' };
@@ -34,6 +103,15 @@ export const PtBr = () => {
     textText[8] = "Clone do Netflix";
     textText[9] = "Roll Dices Game";
     buttonText = "Saiba mais";
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imagePaths.length);
+        }, 100);
+
+        // Limpar o intervalo quando o componente for desmontado ou quando necessário
+        return () => clearInterval(intervalId);
+    }, []);
 
     return (
         <>
@@ -111,6 +189,31 @@ export const PtBr = () => {
                                     </p>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                    <div className="secondArea">
+                        <h2>
+                            Projetos
+                        </h2>
+                        <div className="cards">
+                            <a href="" className="card">
+                                <h3>
+                                    Trabalhos para Terceiros
+                                </h3>
+                                <img src={imagePaths[currentImageIndex]} alt="" />
+                            </a>
+                            <a href="" className="card">
+                                <h3>
+                                    Jogos Digitais
+                                </h3>
+                                <img src={imagePaths[currentImageIndex]} alt="" />
+                            </a>
+                            <a href="" className="card">
+                                <h3>
+                                    Meus Projetos
+                                </h3>
+                                <img src={imagePaths[currentImageIndex]} alt="" />
+                            </a>
                         </div>
                     </div>
                 </div>
