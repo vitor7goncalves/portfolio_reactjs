fe36175185a992eaa2db6594be74fe69e263b3e6
vitor7goncalves <you@example.com>
2025-02-20 15:04:11 -0300

    ...

--- src/components/PageThreeEng.jsx
+++ src/components/PageThreeEng.jsx
@@ -19,10 +19,12 @@ export const PageThreeEng = () => {
     const title = {
         one: 'Storm Wizard Studios Page',
         two: 'Netflix Clone Page',
+        three: 'Boutique Etherea',
     }
     const content = {
         1: "I created the Storm Wizard Studios website to promote my games more professionally. Even though it's a static site, it's enough to keep players informed about the games' news. The site was built with React.js and Vite.js, and the visual design was created by me.",
         2: "The Netflix clone is a website I developed during my initial studies in HTML, CSS, and JavaScript. This project was part of a challenge proposed by the Digital Innovation One Bootcamp, where I took a series of courses and obtained several certificates. The original challenge was to recreate the first page after the client logs in. However, upon completion, I decided to challenge myself even further by creating the homepage and login form from scratch. As a beginner, I faced several challenges, with the main one being making the site responsive.",
+        3: "Boutique Etherea is a project I started developing to practice and challenge my skills in front-end and back-end. The idea is to create a website where a client can display their clothing collections. Users will have access to the products showcased on the site and will be able to create a wishlist with their favorite items. The site features a display of the users' favorite pieces to help understand which items are the most sought after. The website is still incomplete, as it is a large-scale project and demands a lot from me to develop. As with any project in development, there may be some bugs that I am working to fix. For this project, I use Vue.js for the front-end, .Net for the back-end, and WampServer with DBeaver for the database. The front-end part is accessible because it is online, but information that depends on the back-end is unavailable since it runs locally."
     }
     const url = { 1: '', 2: '', 3: '' };
     url[0] = "https://stormwizardmaguspocus.netlify.app";
@@ -64,6 +66,22 @@ export const PageThreeEng = () => {
                         </a>
                         <img className="borderCicleLast" src={Border} alt="" />
                         <iframe className="youtube" src="https://www.youtube.com/embed/Bvrq5t6KX8s?si=cr0U5HJeJ9lJlCpu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
+                    <div className="objs">
+                        <img className="BorderTop" src={BorderTop} alt="Border" />
+                        <div className="text">
+                            <h2>{title.three}</h2>
+                            <p>
+                                {content[3]}
+                            </p>
+                        </div>
+                        <a href={url[2]} className="blueButton ">
+                            <img className="backgroundButton" src={backButton} alt="Background button" />
+                            <img className="buttonImg" src={buttonBlue} alt="Button" />
+                            <p>{text[0]}</p>
+                        </a>
+                        <img className="borderCicleLast" src={Border} alt="" />
+                        <iframe className="youtube" src="https://youtu.be/wjmxXIYhoTQ?si=-tYoycTuhz0awyoM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     </div>
                 </div>
                 <div className="spaceBottomTwo"></div>
--- src/components/PageThreePtBr.jsx
+++ src/components/PageThreePtBr.jsx
@@ -19,14 +19,17 @@ export const PageThreePtBr = () => {
     const title = {
         one: 'Site Storm Wizard Studios',
         two: 'Site Netflix Clone',
+        three: 'Boutique Etherea',
     }
     const content = {
         1: "Eu criei o site Storm Wizard Studios para divulgar meus jogos de forma mais profissional. Mesmo sendo um site estatico é o suficiente para deixar os jogares informados das novidades dos jogos. O site foi criado com React.js e Vite.js e o visual design foi criado por mim.",
         2: "O Netflix clone é um site que desenvolvi durante meus estudos iniciais em HTML, CSS e JavaScript. Este projeto foi parte de um desafio proposto pelo Bootcamp Digital Innovation One, onde participei de uma série de cursos e obtive vários certificados. O desafio original era recriar a primeira página após o cliente efetuar o login. No entanto, após a conclusão, decidi me desafiar ainda mais, criando a página inicial e o formulário de login do zero. Como iniciante, enfrentei diversos desafios, sendo o principal deles tornar o site responsivo.",
+        3: "Boutique Etherea é um projeto que comecei a desenvolver para praticar e desafiar meus conhecimentos em front-end e back-end. A ideia é criar um site onde um cliente possa exibir suas coleções de roupas. Os usuários terão acesso aos produtos exibidos no site e poderão criar uma lista de desejos com as peças que mais gostaram. O site conta com uma exibição das peças favoritas dos usuários para ajudar a entender quais são as mais procuradas. O site ainda está incompleto, já que é um projeto de grande porte e exige muito de mim para desenvolvê-lo. Como em todo projeto em desenvolvimento, podem existir alguns bugs que estou trabalhando para corrigir. Para este projeto, utilizo Vue.js para o front-end, .Net para o back-end e WampServer com DBeaver para o banco de dados. A parte do front-end é acessível, pois está online, mas informações que dependem do back-end estão indisponíveis, já que rodam localmente.",
     }
     const url = { 1: '', 2: '', 3: '' };
     url[0] = "https://stormwizardstudios.netlify.app";
     url[1] = "https://github.com/vitor7goncalves/Netflix-clone";
+    url[2] = "https://boutiqueetherea.netlify.app";
 
     return (
         <>
@@ -64,6 +67,22 @@ export const PageThreePtBr = () => {
                         </a>
                         <img className="borderCicleLast" src={Border} alt="" />
                         <iframe className="youtube" src="https://www.youtube.com/embed/Bvrq5t6KX8s?si=cr0U5HJeJ9lJlCpu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
+                    <div className="objs">
+                        <img className="BorderTop" src={BorderTop} alt="Border" />
+                        <div className="text">
+                            <h2>{title.three}</h2>
+                            <p>
+                                {content[3]}
+                            </p>
+                        </div>
+                        <a href={url[2]} className="blueButton ">
+                            <img className="backgroundButton" src={backButton} alt="Background button" />
+                            <img className="buttonImg" src={buttonBlue} alt="Button" />
+                            <p>{text[0]}</p>
+                        </a>
+                        <img className="borderCicleLast" src={Border} alt="" />
+                        <iframe className="youtube" src="https://youtu.be/wjmxXIYhoTQ?si=-tYoycTuhz0awyoM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     </div>
                 </div>
                 <div className="spaceBottomTwo"></div>
